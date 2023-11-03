3d14b9df284a5fca3fb40158d2b0af6f5072dc4b
Nurlan <nurlanea@code.edu.az>
2023-11-03 18:26:39 +0400

    upload

---
+++ menfimusbet/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] arr = {-12, -29, -33, -56, -70, -84};
+int[] result = line(ref arr);
+
+foreach (int i in result)
+{
+    Console.Write(i + " "); 
+}
+
+int [] line (ref int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+       arr[i]*=(-1);
+    }
+return arr;
+}
+
