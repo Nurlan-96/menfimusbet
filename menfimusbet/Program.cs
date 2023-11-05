02caa9ba41792c5c72377e7a7fef8cf97a935eaa
Nurlan-96 <146638166+Nurlan-96@users.noreply.github.com>
2023-11-05 15:36:58 +0400

    fix

--- menfimusbet/Program.cs
+++ menfimusbet/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] arr = {-12, -29, -33, -56, -70, -84};
+﻿int[] arr = {-12, 29, -33, -56, 70, -84};
 int[] result = line(ref arr);
 
 foreach (int i in result)
@@ -6,12 +6,18 @@ foreach (int i in result)
     Console.Write(i + " "); 
 }
 
-int [] line (ref int [] arr)
+int[] line(ref int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-       arr[i]*=(-1);
+        if (arr[i] < 0)
+        {
+            arr[i] *= (-1);
+        }
+        else
+        {
+            continue;
+        }
     }
-return arr;
+    return arr;
 }
-
